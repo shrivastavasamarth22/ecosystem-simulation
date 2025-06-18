a229722da1b447c65ba17bd100ce0347cdeae337
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2025-06-18 20:02:41 +0530

    Created the World.h header file

---
+++ World.h
@@ -0,0 +1,36 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include <vector>
+#include <memory>
+#include "Animal.h"
+
+class World {
+  private:
+    int width;
+    int height;
+    std::vector<std::unique_ptr<Animal>> animals;
+
+    void cleanup();
+
+  public:
+    World(int w, int h);
+
+    void init(int initial_herbivores, int initial_carnivores);
+    void update();
+    void draw() const;
+    bool isExtinct() const;
+
+    // Getters for other classes to use
+    int getWidth() const {
+      return width;
+    };
+    int getHeight() const {
+      return height;
+    };
+
+    // Helper for Carnivore hunting logic
+    std::vector<Animal*> getHerbivoresNear(int x, int y, int radius);
+};
+
+#endif // WORLD_H
