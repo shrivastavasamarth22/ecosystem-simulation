ff58d0e130568474413c551d4b48fd61db8828bd
Samarth Shrivastava <shrivastavasamarth22@gmail.com>
2025-06-18 19:55:14 +0530

    Made the Animal header file

---
+++ Animal.h
@@ -0,0 +1,49 @@
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <memory>
+
+// Forward declaration to avoid circular include with World.h
+
+class World;
+
+class Animal {
+  protected:
+    int x, y;
+    int energy;
+    int age;
+    bool is_alive;
+    char symbol;
+
+  public:
+    // Constructor 
+    Animal(int start_x, int start_y, int start_energy, char sym);
+
+    // Virtual destructor
+    virtual ~Animal() = default;
+
+    // Pure virtual functions
+    virtual void update(World& world) = 0;
+    virtual std::unique_ptr<Animal> reproduce() = 0;
+
+
+    // Common functions for all animals
+    void move(const World& world);
+    void gainEnergy(int amount);
+    bool isDead() const;
+    void kill();
+
+    // Getters
+    int getX() const {
+      return x;
+    };
+    int getY() const {
+      return y;
+    };
+    int getSymbol() const {
+      return symbol;
+    };
+
+};
+
+#endif // ANIMAL_H
